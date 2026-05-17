437c7a1a744d1331cee6dd1971f94c2f7f418946
Julio Cézar Oliveira Santos <julioSantosDev@proton.me>
2026-05-17 16:39:02 -0300

    Arquivos CSV de identificacao

--- src/main.c
+++ src/main.c
@@ -1,10 +1,14 @@
+<<<<<<< HEAD
 // Módulo main:
 // O módulo principal onde o programa é executado
+=======
+>>>>>>> 266fd76 (Arquivos CSV de identificacao)
 // Arquivo de incialização do projeto. Ese arquivo deve conter somente o menu inicial do projeto e a chamada dos demais módulos.
 // Não insira métodos adicionais ou lógica complexa nesse arquivo.
 
-// Bibliotecas
+// bibliotecas padrao
 #include <stdio.h>
+<<<<<<< HEAD
 #include "../include/ingresso.h"
 #include "../include/persistencia.h"
 #include "../include/vendaIngressos.h"
@@ -56,3 +60,39 @@ int main(){
     printf("Programa encerrado.\n");
     return 0;
 }
+=======
+
+// bibliotecas proprias
+#include "../include/vendaIngressos.h"
+
+int main(){
+	printf("Sistema Geral de Festa Junina\n");
+
+	int escolha;
+
+	while (1){
+		printf("1 - Venda de ingressos(automatizado por CSV)\n2 - Venda de produtos internos(Interativo)\n3 - Gerar relatórios finais\n4 - Encerrar programa\n");
+		printf("Sua escolha: ");
+		scanf("%d", &escolha);
+		
+		printf("\n");
+
+		switch(escolha){
+			case 1:
+				// chamar módulo venda de ingressos
+				vendaIngressos();
+				break;
+			case 2:
+				// chamar módulo venda de produtos
+				break;
+			case 3:
+				// gerar relatórios finais
+				break;
+			case 4:
+				printf("Encerrando programa!");
+				return 0;
+				break;
+		}
+	}
+}
+>>>>>>> 266fd76 (Arquivos CSV de identificacao)
