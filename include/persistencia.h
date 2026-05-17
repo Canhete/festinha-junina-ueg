9117ee12cf69ab1a06959ece3e7a5ce0a767f295
canhete <raphaelcanheteneto@gmail.com>
2026-05-17 14:51:43 -0300

    Resolvido mais conflitos...

--- include/persistencia.h
+++ include/persistencia.h
@@ -8,7 +8,6 @@
 // Bibliotecas
 #include "ingresso.h"
 
-<<<<<<< HEAD
 #define CAMINHO_INGRESSOS "data/ingressos/" // Onde será salvo o arquivo de ingressos
 #define ALUNOS_ANTES_28_CSV "listaAlunosAntes28.csv" // Nome do arquivo de alunos antes do dia 28
 #define ALUNOS_DEPOIS_28_CSV "listaAlunosDepois28.csv" // Nome do arquivo de alunos antes do dia 28
@@ -16,12 +15,5 @@
 // Headers
 int salvarIngresso(Ingresso *ing);
 int carregarIngressos(Ingresso *fila, const char* diretorio, int max);
-=======
-#define CAMINHO_INGRESSOS_CSV "data/ingressos.csv" // Onde será salvo o arquivo de ingressos
-
-// Headers
-int salvarIngresso(Ingresso *ing);
-int carregarIngressos(Ingresso *fila, int max);
->>>>>>> 334cf40b6414fdd4635be9e94d18459dd282e394
 
 #endif
