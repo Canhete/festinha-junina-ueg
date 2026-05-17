36bf2e5356445e7b560cb02893fd45463c3b7daa
Julio Cézar Oliveira Santos <julioSantosDev@proton.me>
2026-05-17 18:52:59 -0300

    Modulo vendaIngressos - adicionando controle de estoque de ingressos

--- include/vendaIngressos.h
+++ include/vendaIngressos.h
@@ -3,6 +3,15 @@
 #ifndef VENDAINGRESSOS_H
 #define VENDAINGRESSOS_H
 
+#define MAX_INGRESSOS 1000
+
+typedef struct {
+    int ingressos;
+    int cervejas;
+    int refrigerantes;
+    int pasteis;
+} ControleEstoque;
+
 int vendaIngressos();
 
 #endif
