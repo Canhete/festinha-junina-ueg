4551e6cab0e58c9316659e6bd7355f5302c36108
guilherme <guilherme.souza@aluno.ueg.br>
2026-05-18 15:20:33 -0300

    Realização do fechamento festa

--- include/fechamentoFesta.h
+++ include/fechamentoFesta.h
@@ -1,11 +1,11 @@
-#ifndef FECHAMENTOFESTA_H
-#define FECHAMENTOFESTA_H
+#ifndef FECHAMENTO_FESTA_H
+#define FECHAMENTO_FESTA_H
 
 #include "pessoa.h"
 
+// Protótipos das funções de fechamento
+Pessoa* carregarParticipantesFechamento(const char* nomeArquivo);
+void emitirRelatorioFechamento(Pessoa* lista);
+int fechamentoFesta();
 
-
-//Funções  
-void fechamentoFesta(Pessoa* lista);
-
-#endif
+#endif // FECHAMENTO_FESTA_H
